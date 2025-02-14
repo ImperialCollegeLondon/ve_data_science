a4ede198e2f8e832d33ea9d589924f56af44de24
David Orme <davidorme@users.noreply.github.com>
2025-02-14 14:22:44 +0000

    System R dependencies justifications

--- system_dependencies.R
+++ system_dependencies.R
@@ -3,9 +3,7 @@
 # for packages used within the project, so there is no need to add packages used in
 # analysis.
 
-# This is required for R support in VSCode
-library(languageserver)
+
+library(languageserver) # Enables code suggestions and linting in VSCode
 library(styler) # Provides code formatting suggestions and auto-formatting
-# The VSCode lintr extension uses this package for linting, but it isn't included as a
-# dependency of lintr so it needs to be explicitly included here.
-library(cyclocomp)
+library(cyclocomp) # Supports code complexity checks in R linting
