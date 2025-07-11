cd10a5afc5f68466b60a658ff6b6374f563fa4e9
Hao Ran Lai <hrlai.ecology@gmail.com>
2025-07-12 11:53:22 +1200

    overhaul model script

--- analysis/litter/chemistry_and_turnover/model.R
+++ analysis/litter/chemistry_and_turnover/model.R
@@ -1,24 +1,23 @@
 #' ---
-#' title: Estimating fungal-to-bacteria ratio from SAFE data
+#' title: Estimating litter stocks from SAFE data
 #'
 #' description: |
-#'     This R script estimates fungal-to-bacteria ratio from SAFE data
+#'     This R script estimates litter stocks (leaf, wood, reproductive)
+#'     from SAFE data
 #'
-#' VE_module: Soil
+#' VE_module: Litter
 #'
 #' author:
 #'   - name: Hao Ran Lai
 #'
-#' status: final
+#' status: wip
 #'
 #' input_files:
-#'   - name: SAFE_Dataset.xlsx
-#'     path: data/primary/soil/fungal_bacteria_ratio
+#'   - name: Ewers_LeafLitter.xlsx
+#'     path: data/primary/litter/
 #'     description: |
-#'       Soil and litter chemistry, soil microbial communities and
-#'       litter decomposition from tropical forest and oil palm dataset by
-#'       Elias Dafydd et al. from SAFE; downloaded from
-#'       https://doi.org/10.5281/zenodo.3929632
+#'       Wet and dry weight of leaf litterfall at SAFE vegetation plots by
+#'       Robert Ewers; downloaded from https://zenodo.org/records/1198587
 #'
 #' output_files:
 #'   - name: NA
@@ -29,50 +28,29 @@
 #' package_dependencies:
 #'     - tidyverse
 #'     - readxl
-#'     - glmmTMB
-#'     - performance
 #'
 #' usage_notes: |
-#'   In the future, it is possible to get a numerically more accurate ratio
-#'   using bootstrap values from the point estimate AND covariances of the
-#'   predict function.
+#'   NA
+#'
 #' ---
 
 library(tidyverse)
 library(readxl)
-library(glmmTMB)
 
 
 
 # Data --------------------------------------------------------------------
 
-litter_compo <-
-  read_xlsx("data/primary/litter/Ewers_LeafLitter.xlsx",
-    sheet = 3,
-    skip = 9
-  ) %>%
-  # use the first survey because only it has litter component weights
-  filter(SurveyNum == 1) %>%
-  mutate_at(vars(starts_with("WW") | starts_with("DW")), as.numeric) %>%
-  mutate(
-    log_days = log(as.numeric(DateCollected - DateSet)),
-    DW.leaf = DW.leaves.photo + DW.leaves.other,
-    DW.reproduction = DW.flower + DW.fruit + DW.seed
-  ) %>%
-  select(
-    Plot, log_days,
-    DW.leaf, DW.wood, DW.reproduction, DW.other
-  ) %>%
-  pivot_longer(
-    cols = starts_with("DW"),
-    names_to = "Type",
-    values_to = "DW"
-  )
+# SAFE has multiple litter dynamics datasets
+# so the first step is to harmonise them into a single dataset
+# the target is a dataframe at least with columns initial litter weight,
+# final litter weight, and time lapsed
 
-litter_nutrient <-
-  read_xlsx("data/primary/soil/fungal_bacteria_ratio/SAFE_Dataset.xlsx",
-    sheet = 4,
-    skip = 9
+# Plowman et al. (2018) https://zenodo.org/records/1220270
+turner19 <-
+  read_xlsx("data/primary/litter/template_Plowman.xlsx",
+    sheet = 3,
+    skip = 5
   ) %>%
   select(
     Plot_ID,
@@ -90,33 +68,8 @@ litter_nutrient <-
   )
 
 
-# Lignin, N and P contents of litter
-
 
 
 
 
 # Model -------------------------------------------------------------------
-
-mod_compo <- glmmTMB(
-  DW ~ 0 + Type + (1 | Plot) + offset(log_days),
-  dispformula = ~ 0 + Type,
-  family = tweedie,
-  data = litter_compo
-)
-
-summary(mod_compo)
-
-exp(fixef(mod_compo)$cond)
-
-
-
-mod_nutrient <- glmmTMB(
-  concentration ~ 0 + chemical +
-    rr(chemical + 0 | location_name, d = 2),
-  dispformula = ~ 0 + chemical,
-  family = lognormal(),
-  data = litter_nutrient
-)
-
-summary(mod_nutrient)
