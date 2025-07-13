cabcd0b28d8621e6e5d273edfd1a06db71e87fae
Hao Ran Lai <hrlai.ecology@gmail.com>
2025-07-13 15:23:58 +1200

    script to mimic soil example data generation in R

--- generation/soil/mimic_ve_example.R
+++ generation/soil/mimic_ve_example.R
@@ -3,3 +3,116 @@
 # data to VE input
 
 # nolint Based on https://pjbartlein.github.io/REarthSysSci/netCDF.html#data-frame-to-array-conversionrectangular-to-raster
+
+
+library(tidyverse)
+library(ncdf4)
+
+
+# coordinates from ve_example
+nx <- 9
+ny <- 9
+displacement <- seq(0, 721, 90)
+
+# a dataframe that mimics the example soil data
+# the functions to generate soil variables are directly copied from the
+# generation script of soil example data
+mimic <-
+  expand.grid(
+    x = displacement,
+    y = displacement
+  ) %>%
+  mutate(gradient = x / 90 * y / 90) %>%
+  mutate(
+    pH_values = 3.5 + 1.00 * gradient / 64.0,
+    bulk_density_values = 1200.0 + 600.0 * gradient / 64.0,
+    clay_fraction_values = 0.27 + 0.13 * gradient / 64.0,
+    lmwc_values = 0.005 + 0.005 * gradient / 64.0,
+    maom_values = 1.0 + 2.0 * gradient / 64.0,
+    bacterial_C_values = 0.0015 + 0.0035 * gradient / 64.0,
+    fungal_C_values = 0.0015 + 0.0035 * gradient / 64.0,
+    pom_values = 0.1 + 0.9 * gradient / 64.0,
+    necromass_values = 0.00015 + 0.00035 * gradient / 64.0,
+    pom_enzyme_values = 0.01 + 0.49 * gradient / 64.0,
+    maom_enzyme_values = 0.01 + 0.49 * gradient / 64.0,
+    don_values = 2.5e-4 + 2.5e-4 * gradient / 64.0,
+    particulate_n_values = 7.5e-4 + 7.5e-4 * gradient / 64.0,
+    maom_n_values = 0.2 + 0.4 * gradient / 64.0,
+    ammonium_values = 1e-3 + 4e-3 * gradient / 64.0,
+    nitrate_values = 1e-3 + 4e-3 * gradient / 64.0,
+    necromass_n_values = 3e-5 + 7e-5 * gradient / 64.0,
+    dop_values = 1e-5 + 1e-5 * gradient / 64.0,
+    particulate_p_values = 3e-5 + 3e-5 * gradient / 64.0,
+    maom_p_values = 0.008 + 0.016 * gradient / 64.0,
+    necromass_p_values = 1.2e-6 + 2.8e-6 * gradient / 64.0,
+    primary_p_values = 0.001 + 0.004 * gradient / 64.0,
+    secondary_p_values = 0.005 + 0.045 * gradient / 64.0,
+    labile_p_values = 2.5e-5 + 2.5e-5 * gradient / 64.0
+  )
+
+# convert dataframe to arrays
+soil_vars <- names(mimic)[-(1:3)]
+array_list <- vector("list", length(soil_vars))
+names(array_list) <- soil_vars
+for (i in soil_vars) {
+  array_list[[i]] <- array(mimic[[i]], dim = c(nx, ny))
+}
+
+# path and file name of netCDF
+ncpath <- "generation/soil/"
+ncname <- "example_soil_data_mimic"
+ncfname <- paste(ncpath, ncname, ".nc", sep = "")
+
+# create and write the netCDF file -- ncdf4 version
+# define dimensions
+xdim <- ncdim_def("x", "m", as.double(displacement))
+ydim <- ncdim_def("y", "m", as.double(displacement))
+
+# define variables
+# can this be simplified with lapply or a loop?
+vardef <- list(
+  ncvar_def("pH_values", "unitless", list(xdim, ydim)),
+  ncvar_def("bulk_density_values", "kg m^-3", list(xdim, ydim)),
+  ncvar_def("clay_fraction_values", "fraction", list(xdim, ydim)),
+  ncvar_def("lmwc_values", "kg C m^-3", list(xdim, ydim)),
+  ncvar_def("maom_values", "kg C m^-3", list(xdim, ydim)),
+  ncvar_def("bacterial_C_values", "kg C m^-3", list(xdim, ydim)),
+  ncvar_def("fungal_C_values", "kg C m^-3", list(xdim, ydim)),
+  ncvar_def("pom_values", "kg C m^-3", list(xdim, ydim)),
+  ncvar_def("necromass_values", "kg C m^-3", list(xdim, ydim)),
+  ncvar_def("pom_enzyme_values", "kg C m^-3", list(xdim, ydim)),
+  ncvar_def("maom_enzyme_values", "kg C m^-3", list(xdim, ydim)),
+  ncvar_def("don_values", "kg N m^-3", list(xdim, ydim)),
+  ncvar_def("particulate_n_values", "kg N m^-3", list(xdim, ydim)),
+  ncvar_def("maom_n_values", "kg N m^-3", list(xdim, ydim)),
+  ncvar_def("ammonium_values", "kg N m^-3", list(xdim, ydim)),
+  ncvar_def("nitrate_values", "kg N m^-3", list(xdim, ydim)),
+  ncvar_def("necromass_n_values", "kg N m^-3", list(xdim, ydim)),
+  ncvar_def("dop_values", "kg P m^-3", list(xdim, ydim)),
+  ncvar_def("particulate_p_values", "kg P m^-3", list(xdim, ydim)),
+  ncvar_def("maom_p_values", "kg P m^-3", list(xdim, ydim)),
+  ncvar_def("necromass_p_values", "kg P m^-3", list(xdim, ydim)),
+  ncvar_def("primary_p_values", "kg P m^-3", list(xdim, ydim)),
+  ncvar_def("secondary_p_values", "kg P m^-3", list(xdim, ydim)),
+  ncvar_def("labile_p_values", "kg P m^-3", list(xdim, ydim))
+)
+
+# create netCDF file and put arrays
+ncout <- nc_create(ncfname, vardef, force_v4 = TRUE)
+
+# put variables
+for (i in seq_along(soil_vars)) {
+  ncvar_put(ncout, vardef[[i]], array_list[[i]])
+}
+
+# add global attributes
+ncatt_put(
+  ncout, 0, "description",
+  "Soil data for dummy Virtual Ecosystem model (mimicked in R)"
+)
+
+# Get a summary of the created file:
+ncout
+
+# close the file, writing data to disk
+nc_close(ncout)
