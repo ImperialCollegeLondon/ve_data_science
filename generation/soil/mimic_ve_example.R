237bf8fbb99b925f14a3d026e6d31206a441d17d
Hao Ran Lai <hrlai.ecology@gmail.com>
2025-07-13 14:07:28 +1200

    test nolint for long URLs

---
+++ generation/soil/mimic_ve_example.R
@@ -0,0 +1,5 @@
+# Testing script to convert dataframe to netCDF
+# The goal is to mimic an ecologist's workflow of converting their field
+# data to VE input
+
+# nolint Based on https://pjbartlein.github.io/REarthSysSci/netCDF.html#data-frame-to-array-conversionrectangular-to-raster
