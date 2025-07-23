0d485b4528883e7e6e31e0c07c1499d71dba806b
David Orme <davidorme@users.noreply.github.com>
2025-07-23 15:30:39 +0100

    Maliau data reprojection for ERA5 downloads

---
+++ analysis/abiotic/data_download/maliau_era5_reproject.py
@@ -0,0 +1,74 @@
+# noqa: D100
+from pathlib import Path
+
+import tomllib
+import xarray
+from rasterio import Affine
+from rasterio.crs import CRS
+from rasterio.warp import Resampling
+
+# Define the projections to be used
+wgs84_crs = CRS.from_epsg(4326)
+utm50N_crs = CRS.from_epsg(32650)
+
+# Load the destination grid details
+with open("../../core/maliau_grid_definition.toml", "rb") as maliau_grid_file:
+    utm50N_grid_details = tomllib.load(maliau_grid_file)
+
+# Define the XY shape of the data in the destination dataset
+dest_shape = (
+    utm50N_grid_details["cell_nx"],
+    utm50N_grid_details["cell_ny"],
+)
+
+# Define the affine matrix giving the coordinates of pixels in the destination dataset
+dest_transform = Affine(
+    utm50N_grid_details["res"],
+    0,
+    utm50N_grid_details["min_x"],
+    0,
+    utm50N_grid_details["res"],
+    utm50N_grid_details["min_y"],
+)
+
+
+# Set the source directory
+source_directory = Path("../../../data/primary/abiotic/era5_land_monthly/")
+
+downloaded_variables = [
+    "2m_temperature",
+    "2m_dewpoint_temperature",
+    "surface_pressure",
+    "10m_u_component_of_wind",
+    "total_precipitation",
+    "surface_runoff",
+]
+
+# Store dictionary for reprojected data
+reprojected_data = dict()
+
+for var in downloaded_variables:
+    # Get the target file
+    source_filename = source_directory / f"ERA5_{var}_Maliau_2010_2020.nc"
+
+    # Open the ERA5 dataset in WGS84 and and set the CRS manually because it is not
+    # set in the file. Do not decode times from CF to np.datetime64, because we'd have
+    # to convert back to write the file.
+    era5_data = xarray.open_dataset(
+        source_filename,
+        engine="rasterio",
+        decode_times=False,
+    )
+    era5_data = era5_data.rio.write_crs(wgs84_crs)
+
+    # Use the rasterio accessor tools to reproject the data
+    reprojected_data[var] = era5_data.rio.reproject(
+        dst_crs=utm50N_crs,
+        shape=dest_shape,
+        transform=dest_transform,
+        resampling=Resampling.nearest,
+    )
+
+# Merge the variables into a single Dataset and save to file
+era5_UTM50N = xarray.merge(reprojected_data.values())
+era5_UTM50N.to_netcdf(source_directory / "ERA5_Maliau_2010_2020_UTM50N.nc")
