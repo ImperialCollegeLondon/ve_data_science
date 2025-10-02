6474cdb771b788346c43451c8034688043410e20
Hao Ran Lai <hrlai.ecology@gmail.com>
2025-10-02 21:46:13 +1300

    draft model switching to the simplified beta, see detailed rationale at https://github.com/ImperialCollegeLondon/ve_data_science/pull/109#issuecomment-3359910782

--- analysis/soil/n_fixation/model_free_living_nitrogenase.R
+++ analysis/soil/n_fixation/model_free_living_nitrogenase.R
@@ -1,14 +1,17 @@
 library(tidyverse)
 library(readxl)
+library(quantreg)
 library(brms)
-
-# Functions to fit candidate models from Kontopoulos et al. (2024)
-# https://doi.org/10.1038/s41467-024-53046-2
-# I will source it directly from their GitHub repo
-source("https://raw.githubusercontent.com/dgkontopoulos/Kontopoulos_et_al_83_TPC_models_2024/refs/heads/main/Code/TPC_fitting_functions.R") # nolint
 
 
 # Data --------------------------------------------------------------------
+# Biological nitrogen fixation dataset published by Reis Ely et al. (2025)
+# https://doi.org/10.5066/P1MFBVHK
+bnf_free_living <-
+  read_csv("data/primary/soil/n_fixation/BNF_AREA.csv") %>%
+  left_join(read_csv("data/primary/soil/n_fixation/SITE.csv")) %>%
+  # filter to "soil" which is free-living microbes
+  filter(niche == "Soil")
 
 # Nitrogenase activity data by Houlton et al. 2008, who did not publish their
 # data but was later extracted by Deutsch et al. 2024
@@ -18,79 +21,60 @@ houlton <-
     "data/primary/soil/n_fixation/1-s2.0-S0966842X23003566-mmc1.xlsx",
     range = "A1:C124"
   ) %>%
-  filter(Source == "Houlton2008") %>%
-  # some of the extracted normalised rates are >1, probably due to accuracy
-  # error during visual extraction, I will just cap them to 0.999
-  mutate(
-    Rate = ifelse(`Rate (normalized)` > 1, 0.999, `Rate (normalized)`),
-    .keep = "unused"
-  ) %>%
-  # scale temperature
-  mutate(T_s = Temperature / sd(Temperature))
+  filter(Source == "Houlton2008")
+
 
 
 # Model fitting ------------------------------------------------------------
 
-# Instead fof fitting all 83 candidate models, I will only fit the top ten
-# models that Kontopoulos et al. have found to be best for physiological traits
+# First estimate the maximum or peak fixation rate of free-living organisms
+# by "maximum" I will actually estimate it using the 95th percentile to be more
+# conservative
+mod_max_rate <- rq(
+  BNF_final ~ 1,
+  data = bnf_free_living,
+  tau = 0.95
+)
 
-# Ashrafi II
-ashrafi2 <-
-  bf(Rate ~ a + b * T_s^(3 / 2) - c * T_s^2, nl = TRUE) +
+# the estimated peak fixation rate
+peak_rate <- as.vector(mod_max_rate$coefficients)
+
+# multiply the estimated peak rate to the Houlton dataset to "denormalise" them
+houlton <-
+  houlton %>%
+  mutate(Rate = `Rate (normalized)` * peak_rate)
+
+# Fit the simplified beta type equation to the Houlton dataset
+simplified_beta <-
+  bf(Rate ~ rho * (a - Temperature / 10) * (Temperature / 10)^b,
+    nl = TRUE
+  ) +
+  nlf(a ~ exp(loga)) +
   nlf(b ~ exp(logb)) +
-  nlf(c ~ exp(logc)) +
-  lf(a + logb + logc ~ 1)
+  nlf(rho ~ exp(logrho)) +
+  lf(loga + logb + logrho ~ 1)
 
-# Second-order polynomial
-quadratic <-
-  bf(Rate ~ 1 + T_s + I(T_s^2))
+# priors
+priors <-
+  prior(normal(1.5, 0.5), class = "b", nlpar = "loga") +
+  prior(normal(0.4, 0.5), class = "b", nlpar = "logb") +
+  prior(normal(0, 0.5), class = "b", nlpar = "logrho")
 
-# Atkin
-atkin <-
-  bf(Rate ~ B0 * (a - b * T_s)^T_s / 10)
-
-# Gaussian
-Gaussian <-
-  bf()
-
-# Janisch I
-janisch1 <-
-  bf()
-
-# Ashrafi I
-ashrafi1 <-
-  bf()
-
-# Mitchell--Angilletta
-mitchell_angilletta <-
-  bf()
-
-# Eubank
-eubank <-
-  bf()
-
-# Taylor--Sexton (doesn't fit successfully)
-taylor_sexton <-
-  bf()
-
-# Analytis--Kontodimas
-analytis_kontodimas <-
-  bf()
-
-#
-mod_ashrafi2 <- brm(
-  ashrafi2,
-  family = Beta(),
+# fit
+mod_simplified_beta <- brm(
+  simplified_beta,
+  family = gaussian(),
   data = houlton,
-  prior = prior(normal(-5, 1), class = "b", nlpar = "a") +
-    prior(normal(1.8, 1), class = "b", nlpar = "logb") +
-    prior(normal(1.5, 1), class = "b", nlpar = "logc"),
+  prior = priors,
   cores = 4
 )
 
-mod_quadratic <- brm(
-  quadratic,
-  family = Beta(),
-  data = houlton,
-  cores = 4
+bayesplot::mcmc_trace(mod_simplified_beta, regex_pars = "^b_")
+plot(
+  conditional_effects(
+    mod_simplified_beta,
+    method = "posterior_epred"
+  ),
+  points = TRUE
 )
+summary(mod_simplified_beta)
