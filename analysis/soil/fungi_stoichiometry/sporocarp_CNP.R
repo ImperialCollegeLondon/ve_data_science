fba4d61e64920e8deb5d2f971397e10101edc57c
Hao Ran Lai <hrlai.ecology@gmail.com>
2025-10-03 13:30:55 +1300

    updated models to estimate fruiting body stoichiometry

--- analysis/soil/fungi_stoichiometry/sporocarp_CNP.R
+++ analysis/soil/fungi_stoichiometry/sporocarp_CNP.R
@@ -2,9 +2,8 @@
 #| title: Calculate the C:N and C:P ratios of fungal fruiting body (sporocarp)
 #|
 #| description: |
-#|     This R script converts the C:N:P molar stoichiometry from a meta-analysis
-#|     to mass stoichiometry. I am not doing any analysis and will simply rely
-#|     on the estimated median values from the study.
+#|     This R script estimates the C, N and P mass fraction from a global
+#|     dataset and then estimates the C:N and C:P ratio of fruiting body.
 #|
 #| virtual_ecosystem_module: Soil
 #|
@@ -14,22 +13,78 @@
 #| status: final
 #|
 #| input_files:
+#|   - name: Sporocarp+chemistry.xlsx
+#|     path: data/primary/soil/fungi_stoichiometry
+#|     description: |
+#|       Saprotrophic and ectomycorrhizal fungal sporocarp stoichiometry
+#|       across temperate rainforests by Kranabetter et al. (2018)
+#|       https://doi.org/10.1111/nph.15380; downloaded from
+#|       https://doi.org/10.5061/dryad.1d92k70
 #|
 #| output_files:
+#|   - name: CN_CP_ratio.csv
+#|     path: data/derived/soil/fungi_stoichiometry
+#|     description: |
+#|       Estimated C:N and C:P ratio for fungal fruiting body
 #|
 #| package_dependencies:
-#|     - biogas
+#|     - tidyverse
+#|     - readxl
+#|     - glmmTMB
 #|
 #| usage_notes: |
-#|   This is a global meta-analysis that should suffice at this stage. In the
-#|   future we should also check a slightly newer study (but on saprotrophic and
-#|   ectomycorrhizal fungi only): https://doi.org/10.1111/nph.15380.
+#|     The dataset covers only saprotrophic and EM fungi, but I think they are
+#|     good enough for us at this stage. There are not a lot of stoichiometry
+#|     specifically for the fruiting body in the literature.
 #| ---
 
-library(biogas)
+library(tidyverse)
+library(readxl)
+library(glmmTMB)
 
-# https://doi.org/10.3389/fmicb.2017.01281
-# Median C:N:P stoichiometry for fungi was 250:16:1 (molar)
-# convert molar to mass ratios
-CN_ratio <- 250 * molMass("C") / 16 * molMass("N")
-CP_ratio <- 250 * molMass("C") / 1 * molMass("P")
+
+# Data --------------------------------------------------------------------
+
+sporocarp <-
+  read_excel(
+    "data/primary/soil/fungi_stoichiometry/Sporocarp+chemistry.xlsx",
+    range = "A5:G151"
+  ) %>%
+  rename(C = `%...5`, N = `%...6`, P = `%...7`) %>%
+  mutate_at(vars(C, N, P), ~ . / 100)
+
+
+# Model -------------------------------------------------------------------
+
+# separate models for C, N and P
+mod_C <- glmmTMB(
+  C ~ 1 + (1 | Guild) + (1 | Plot) + (1 | `Site type`),
+  family = beta_family(),
+  data = sporocarp
+)
+
+mod_N <- glmmTMB(
+  N ~ 1 + (1 | Guild) + (1 | Plot) + (1 | `Site type`),
+  family = beta_family(),
+  data = sporocarp
+)
+
+mod_P <- glmmTMB(
+  P ~ 1 + (1 | Guild) + (1 | Plot) + (1 | `Site type`),
+  family = beta_family(),
+  data = sporocarp
+)
+
+# calculate C:N and C:P from estimated mean C, N and P
+C_est <- plogis(fixef(mod_C)$cond)
+N_est <- plogis(fixef(mod_N)$cond)
+P_est <- plogis(fixef(mod_P)$cond)
+
+CN_est <- C_est / N_est
+CP_est <- C_est / P_est
+
+# save output
+write_csv(
+  tibble(CN = CN_est, CP = CP_est),
+  "data/derived/soil/fungi_stoichiometry/CN_CP_ratio.csv"
+)
