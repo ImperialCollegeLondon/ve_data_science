cba5f1bfa5cf8806e10fea839d0dc29aa59f31d5
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-03-03 15:59:42 +0800

    add more covariates

--- analysis/soil/initialisation/model_safe.R
+++ analysis/soil/initialisation/model_safe.R
@@ -10,22 +10,42 @@ library(modelr)
 
 # Data --------------------------------------------------------------------
 
-maliau <-
-  parseTOML("data/sites/maliau_site_definition.toml")
+# Predictors
 
-# TODO update path to match abiotic module input
-# elevation
+# Elevation
 elev <-
   rast("data/primary/abiotic/STRM_data_elevation/SRTM_UTM50N_processed.tif")
-# TODO climate
-tp <- 
-  rast("data/primary/abiotic/era5_land_monthly/ERA5_monthly_2010_2020_SAFE_big.nc",
-       subds = "tp") %>% 
-  mean(., na.rm = TRUE) %>%
-  project(., crs(elev))
-# TODO vegetation
-vege <- 
-  rast("data/primary/soil/nutrient/Danum_acd.tif")
+
+# Topography
+topo <-
+  rast("data/primary/abiotic/SAFE_topographic_roughness/SRTM_UTM50N_TRI_Wilson2007.tif")
+
+# Climate
+# tp <- 
+#   rast("data/primary/abiotic/era5_land_monthly/ERA5_monthly_2010_2020_SAFE_big.nc",
+#        subds = "tp") %>% 
+#   mean(., na.rm = TRUE) %>%
+#   project(., crs(elev))
+
+# Hydrology
+hydro <- 
+  rast("data/primary/abiotic/SRTM_hydrology/SRTM_Log_Flow_Accum.tif")
+
+# LiDAR
+acd <- 
+  merge(
+    rast("data/primary/plant/remote_biomass/Danum_acd.tif"),
+    rast("data/primary/plant/remote_biomass/Maliau_acd.tif"),
+    rast("data/primary/plant/remote_biomass/SAFE_acd.tif")
+  )
+
+# Biomass
+evi <-
+  rast("data/primary/plant/remote_biomass/Sabah_EVI_2024_Q1.tif")
+
+# location details
+maliau <-
+  parseTOML("data/sites/maliau_grid_definition_100m.toml")
 
 extra_locations <-
   tribble(
@@ -48,16 +68,22 @@ location <-
   as.data.frame() %>%
   mutate(location = location_sf$location)
 
+# soil variables
 soil <-
   read_xlsx(
     "data/primary/soil/nutrient/SAFE_soil_nutrient_data.xlsx",
     sheet = 3,
-    skip = 5
+    skip = 4
   ) %>%
   left_join(location, by = join_by("plot_code" == "location")) %>%
   mutate(
-    elevation = extract(elev, .[, c("X", "Y")])[, "SRTM_UTM50N_processed"],
-    tp = extract(tp, .[, c("X", "Y")])[, "mean"])
+    elev = extract(elev, .[, c("X", "Y")])[, "SRTM_UTM50N_processed"],
+    topo = extract(topo, .[, c("X", "Y")])[, "SRTM_UTM50N_TRI_Wilson2007"],
+    hydro = extract(hydro, .[, c("X", "Y")])[, "SRTM_Log_Flow_Accum"],
+    acd = extract(acd, .[, c("X", "Y")])[, "Danum_acd"],
+    evi = extract(evi, .[, c("X", "Y")])[, "EVI"]
+  ) %>% 
+  drop_na(elev, topo, hydro, acd, evi)
 
 soil_vars <-
   c(
@@ -79,7 +105,11 @@ rownames(soil_mat) <- soil$plot_code
 # TODO scale covariates
 soil_scaled <- 
   soil %>% 
-  mutate_at(vars(elevation, tp), ~ as.numeric(scale(.)))
+  mutate_at(vars(elev, topo, hydro, acd, evi), 
+            ~as.numeric(scale(.)))
+
+
+
 
 # Model -------------------------------------------------------------------
 
@@ -89,7 +119,7 @@ num_basis <- 25
 
 # basis functions
 basis_func <-
-  mrts(soil[, c("X", "Y")], num_basis) %>%
+  mrts(soil_scaled[, c("X", "Y")], num_basis) %>%
   as.matrix() %>%
   # Remove the first intercept column
   {
@@ -101,7 +131,7 @@ tic <- proc.time()
 fitcbfm <-
   CBFM(
     y = soil_mat,
-    formula = ~ 1 + elevation + tp,
+    formula = ~ 1 + elev + topo + hydro + acd + evi,
     data = soil_scaled,
     B_space = basis_func,
     family = gaussian(),
@@ -131,12 +161,20 @@ maliau_grid <-
 
 maliau_dat <-
   maliau_grid %>%
-  mutate(elevation = extract(elev, .[, c("X", "Y")])$SRTM_UTM50N_processed,
-         tp = extract(tp, .[, c("X", "Y")])[, "mean"]) %>% 
-  mutate(elevation = (elevation - mean(soil$elevation)) / sd(soil$elevation),
-         tp = (tp - mean(soil$tp)) / sd(soil$tp))
+  mutate(
+    elev = extract(elev, .[, c("X", "Y")])[, "SRTM_UTM50N_processed"],
+    topo = extract(topo, .[, c("X", "Y")])[, "SRTM_UTM50N_TRI_Wilson2007"],
+    hydro = extract(hydro, .[, c("X", "Y")])[, "SRTM_Log_Flow_Accum"],
+    # acd = extract(acd, .[, c("X", "Y")])[, "Danum_acd"],
+    acd = mean(soil$acd),
+    evi = extract(evi, .[, c("X", "Y")])[, "EVI"]
+  ) %>% 
+  mutate(elev = (elev - mean(soil$elev)) / sd(soil$elev),
+         topo = (topo - mean(soil$topo)) / sd(soil$topo),
+         hydro = (hydro - mean(soil$hydro)) / sd(soil$hydro),
+         acd = (acd - mean(soil$acd)) / sd(soil$acd),
+         evi = (evi - mean(soil$evi)) / sd(soil$evi))
 
-# this should not be recreated???
 maliau_basis <-
   mrts(soil[, c("X", "Y")], num_basis) %>%
   predict(newx = maliau_grid[, c("X", "Y")]) %>% 
