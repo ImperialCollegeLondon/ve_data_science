6ece756e33ca89d34f7c61f8dfd376fa23200868
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-03-06 14:52:02 +0800

    calculate aboveground litter nutrients following https://github.com/ImperialCollegeLondon/ve_data_science/issues/110#issuecomment-4009922990

--- analysis/litter/nutrient_pool/litter_lignin.R
+++ analysis/litter/nutrient_pool/litter_lignin.R
@@ -38,62 +38,45 @@
 
 library(tidyverse)
 library(readxl)
-library(mvtnorm)
-library(mclust)
 
 
-chem_leaf <-
-  read_xlsx("data/primary/litter/Both_litter_decomposition_experiment.xlsx",
-    sheet = 3,
-    skip = 7
-  ) %>%
-  # convert lignin from mass/mass to g C/g C
-  # the lignin C content = 62.5% comes from
-  # Martin et al. (2021) DOI: 10.1038/s41467-021-21149-9
-  mutate(lignin = lignin_recalcitrants * 0.625 / C_perc) %>%
-  select(
-    litter_type,
-    C.N, C.P, lignin,
-  )
-
-chem_leaf_mat <- as.matrix(chem_leaf[, -1])
-rownames(chem_leaf_mat) <- chem_leaf$litter_type
-
-mod_chem <- mvn("Ellipsoidal", chem_leaf_mat)
-
-n_sim <- 1000
-sim_chem <-
-  rmvnorm(
-    n_sim,
-    mod_chem$parameters$mean,
-    mod_chem$parameters$variance$Sigma
-  )
-colnames(sim_chem) <- colnames(chem_leaf_mat)
 
 decay_param <- read_csv("data/derived/litter/turnover/decay_parameters.csv")
-
 logitfM <- decay_param$value[decay_param$Parameter == "logitfM"]
 sN <- decay_param$value[decay_param$Parameter == "sN"]
 sP <- decay_param$value[decay_param$Parameter == "sP"]
 
-sim_chem_df <-
-  as_tibble(sim_chem) %>%
+# Ratio of structural vs metabolic C:N and C:P (r in the CENTURY model)
+# this is from Kirschbaum and Paul (2002) Soil Biology and Biochemistry
+# Since the structural and metabolic pools are abstract pools (unfortunately)
+# there is no reason to modify this abstract ratio
+r_century <- 5
+
+
+
+
+litter <-
+  read_xlsx("data/primary/litter/Both_litter_decomposition_experiment.xlsx",
+    sheet = 3,
+    skip = 7
+  ) |>
+  # convert lignin from mass/mass to g C/g C
+  # the lignin C content = 62.5% comes from
+  # Martin et al. (2021) DOI: 10.1038/s41467-021-21149-9
+  mutate(lignin = lignin_recalcitrants * 0.625 / C_perc) |>
+  select(
+    litter_type,
+    C.N, C.P, lignin,
+  ) |>
+  # calculate metabolic fraction
   mutate(fm = plogis(
     logitfM - lignin * (sN * C.N + sP * C.P)
-  ))
-
-n_sim_pool <- 1000
-n_ratio <- numeric(n_sim_pool)
-p_ratio <- numeric(n_sim_pool)
-lignin_ratio <- numeric(n_sim_pool)
-
-for (i in seq_len(n_sim_pool)) {
-  pool <- rbinom(length(sim_chem_df$fm), 1, sim_chem_df$fm)
-  pool_CN <- tapply(sim_chem_df$C.N, pool, mean)
-  pool_CP <- tapply(sim_chem_df$C.P, pool, mean)
-  pool_L <- tapply(sim_chem_df$lignin, pool, mean)
-
-  n_ratio[i] <- pool_CN[1] / pool_CN[2]
-  p_ratio[i] <- pool_CP[1] / pool_CP[2]
-  lignin_ratio[i] <- pool_L[1] / pool_L[2]
-}
+  )) |>
+  # calculate metabolic and structural nutrients
+  # see rearranged equation on the litter theory documentation
+  # nolint https://virtual-ecosystem.readthedocs.io/en/latest/virtual_ecosystem/theory/soil/litter_theory.html#split-of-nutrient-inputs-between-pools
+  mutate(
+    C.N_metabolic = C.N / (r_century + fm * (1 - r_century)),
+    C.P_metabolic = C.P / (r_century + fm * (1 - r_century)),
+    C.N_structural = r_century * C.N_metabolic,
+    C.P_structural = r_century * C.P_metabolic)
