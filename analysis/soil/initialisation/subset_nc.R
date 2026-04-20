d1da919f04ccd133d2a9e120a5ad2b2581c48063
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-04-20 12:37:03 +0800

    document function

--- analysis/soil/initialisation/subset_nc.R
+++ analysis/soil/initialisation/subset_nc.R
@@ -1,3 +1,16 @@
+#' Subset netCDF data used by the Virtual Ecosystem
+#'
+#' Subset netCDF input data using latitudinal and longitudinal limits.
+#'
+#' @param nc Path and filename to the netCDF file to be subsetted.
+#' @param ll_x Lower limit of the x or longitudinal dimension.
+#' @param ll_y Lower limit of the y or longitudinal dimension.
+#' @param ur_x Upper limit of the x or longitudinal dimension.
+#' @param ur_y Upper limit of the y or longitudinal dimension.
+#' @param ... Additional arguments passed to convert_array_to_nc()
+#'
+#' @returns A subset netCDF file written to disk as per filename when close.nc is TRUE.
+
 subset_nc <- function(nc, ll_x, ll_y, ur_x, ur_y, ...) {
   # filter data to region of interest
   data_subset <-
