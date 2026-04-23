5c768d26504ee1271d2250ea7e6958ff267b59f9
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-04-23 23:23:54 +0800

    draft script to record runtime

---
+++ analysis/troubleshoot/runtime_per_cell/runtime.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(ve.utils)
+library(tictoc)
+
+
+config_dir <- "data/scenarios/runtime_per_cell/config"
+configs <- list.dirs(config_path, full.names = FALSE, recursive = FALSE)
+
+out_dir <- "data/scenarios/runtime_per_cell/out"
+outs <- paste0(out_dir, str_replace(configs, "config", "out"))
+
+runtime <- numeric(length(configs))
+names(runtime) <- str_remove(configs, "config_")
+
+for (i in seq_along(outs)) {
+  if (!dir.exists(outs[i])) {
+    dir.create(outs[i])
+  }
+
+  tic()
+  ve_run(
+    paste0(config_dir, "/", configs[i]),
+    outs[i],
+    paste0(outs[i], "/logfile.log")
+  )
+  runtime[i] <- toc()
+}
