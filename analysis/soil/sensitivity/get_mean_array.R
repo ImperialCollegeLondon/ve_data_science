a09ea24d068f24e6390865a044c730639350345b
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-04-25 22:17:11 +0800

    Merge branch '277-refactor-soil-litter-netcdf-generation-code' of https://github.com/ImperialCollegeLondon/ve_data_science into 254-sensitivity-analysis-of-soil-and-litter-initial-values

--- analysis/soil/sensitivity/get_mean_array.R
+++ analysis/soil/sensitivity/get_mean_array.R
@@ -1,16 +1,16 @@
-get_mean_array <- function(nc, ll_x, ll_y, ur_x, ur_y) {
+get_mean_array <- function(nc, ...) {
   # convert netCDF to array
-  # subsetting is optional; if the x and y bounds are set to the same of the
-  # original data, then no subsetting is done
-  dat_array <- subset_array(nc, ll_x, ll_y, ur_x, ur_y)
+  # subsetting is optional; if the x and y bounds, or cell_id, are set to the
+  # same as the original data, then no subsetting is done
+  dat_array <- subset_array(nc, ...)
 
   # get the mean array for all state variables
   lapply(dat_array, function(var) {
     # collect dimension names and format them for the new mean array
     dim_names <- names(dimnames(var))
 
-    # marginalise over any dimension(s) that are not x and y
-    dim_to_marginalise <- which(!dim_names %in% c("x", "y"))
+    # marginalise over any dimension(s) that are not x, y or cell_id
+    dim_to_marginalise <- which(!dim_names %in% c("x", "y", "cell_id"))
 
     # separate attribute collection when there is vs. is not any dimension
     # to marginalise across
@@ -29,14 +29,9 @@ get_mean_array <- function(nc, ll_x, ll_y, ur_x, ur_y) {
       mean_array <- array(mean_array, dim = dim_new, dimnames = dimnames_new)
     } else {
       # when there is no other dimension to marginalise across, the output
-      # should simply be a 1x1 matrix
+      # should simply be a scalar array
       dimnames_new <- lapply(dimnames(var), function(names) names[1])
-      mean_array <- matrix(
-        mean(var),
-        nrow = 1,
-        ncol = 1,
-        dimnames = dimnames_new
-      )
+      mean_array <- array(mean(var), dim = 1, dimnames = dimnames_new)
     }
 
     return(mean_array)
