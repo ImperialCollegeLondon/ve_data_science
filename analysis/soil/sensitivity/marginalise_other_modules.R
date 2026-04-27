52118809909d844b614b3e7a5296d5ef15d3ab66
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-04-27 15:46:01 +0800

    remove unused lines

--- analysis/soil/sensitivity/marginalise_other_modules.R
+++ analysis/soil/sensitivity/marginalise_other_modules.R
@@ -5,17 +5,7 @@ library(tidync)
 library(purrr)
 source("analysis/soil/initialisation/convert_array_to_nc.R")
 source("analysis/soil/initialisation/get_all_variables.R")
-source("analysis/soil/sensitivity/get_mean_array.R")
-
-
-# Maliau site metadata ----------------------------------------------------
-
-maliau <-
-  parseTOML("data/derived/site/maliau/maliau_grid_definition_100m.toml")
-ll_x <- maliau$ll_x
-ll_y <- maliau$ll_y
-ur_x <- maliau$ur_x
-ur_y <- maliau$ur_y
+source("analysis/soil/sensitivity/summarise_spatial.R")
 
 
 # Generate mean arrays ---------------------------------------------------
