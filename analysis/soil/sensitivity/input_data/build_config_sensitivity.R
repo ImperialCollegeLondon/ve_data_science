c3e9a4e1e25504d5f01f1b64ba0ff754b8e91ec7
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-04-29 07:54:51 +0800

    fix relative paths

--- analysis/soil/sensitivity/input_data/build_config_sensitivity.R
+++ analysis/soil/sensitivity/input_data/build_config_sensitivity.R
@@ -17,13 +17,25 @@ core_grid <-
   list_modify(cell_nx = 1, cell_ny = 1)
 
 # input data paths for core.data.variable
-data_paths <- collect_data_paths(
-  plants = "../data/plant_input_data_Maliau_50x50_mean.nc",
-  climate = "../data/era5_maliau_2010_2020_100m_mean.nc",
-  elevation = "../data/elevation_maliau_2010_2020_100m_mean.nc",
-  soil = "../data/soil_litter_data.nc",
-  litter = "../data/soil_litter_data.nc"
+# first list the soil and litter data files to loop over
+data_paths_soil_litter <- paste0(
+  "../../data/",
+  list.files(
+    "data/scenarios/sensitivity_soil_litter/data",
+    "soil_litter_data",
+  )
 )
+data_paths <-
+  data_paths_soil_litter |>
+  map(\(path) {
+    collect_data_paths(
+      plants = "../../data/plant_input_data_Maliau_50x50_mean.nc",
+      climate = "../../data/era5_maliau_2010_2020_100m_mean.nc",
+      elevation = "../../data/elevation_maliau_2010_2020_100m_mean.nc",
+      soil = path,
+      litter = path
+    )
+  })
 
 # new values for plants.constants
 plants_contants <-
@@ -36,48 +48,65 @@ plants_contants <-
 # These new values reflect the Maliau scenarios
 
 # core config
-core <- list(
-  grid = core_grid,
-  timing = list(
-    start_date = "2010-01-01",
-    update_interval = "1 month",
-    run_length = "11 years"
-  ),
-  data_output_options = list(
-    save_initial_state = FALSE,
-    save_continuous_data = FALSE
-  ),
-  data = list(variable = data_paths)
-)
+core_list <-
+  map(
+    data_paths,
+    \(data_path) {
+      list(
+        grid = core_grid,
+        timing = list(
+          start_date = "2010-01-01",
+          update_interval = "1 month",
+          run_length = "11 years"
+        ),
+        data_output_options = list(
+          save_initial_state = FALSE,
+          save_continuous_data = FALSE
+        ),
+        data = list(variable = data_path)
+      )
+    }
+  )
 
 # plants config
 plants <- list(
-  cohort_data_path = "../data/plant_cohort_data_Maliau_50x50.csv",
-  pft_definitions_path = "../data/plant_pft_definitions_Maliau_50x50.csv",
+  cohort_data_path = "../../data/plant_cohort_data_Maliau_50x50.csv",
+  pft_definitions_path = "../../data/plant_pft_definitions_Maliau_50x50.csv",
   constants = as.list(plants_contants)
 )
 
 # animal config
 animal <- list(
-  functional_group_definitions_path = "../data/animal_functional_groups_Maliau_level1.csv",
+  functional_group_definitions_path = "../../data/animal_functional_groups_Maliau_level1.csv",
   cohort_data_export = list(enabled = FALSE)
 )
 
 
 # Build configuration files ----------------------------------------------
 
-build_config(
-  requested_modules = c(
-    "core",
-    "abiotic_simple",
-    "hydrology",
-    "plants",
-    "animal",
-    "soil",
-    "litter"
-  ),
-  core = core,
-  plants = plants,
-  animal = animal,
-  path = "data/scenarios/sensitivity_soil_litter/config"
-)
+iwalk(core_list, \(x, idx) {
+  config_dir <- paste0("data/scenarios/sensitivity_soil_litter/config/", idx)
+  if (!dir.exists(config_dir)) {
+    dir.create(config_dir)
+  }
+  build_config(
+    requested_modules = c(
+      "core",
+      "abiotic_simple",
+      "hydrology",
+      "plants",
+      "animal",
+      "soil",
+      "litter"
+    ),
+    core = x,
+    plants = plants,
+    animal = animal,
+    path = config_dir
+  )
+  # Copy over soil microbial config that does not change across scenarios
+  file.copy(
+    "data/scenarios/maliau/maliau_1/config/soil_microbial_groups.toml",
+    config_dir
+  )
+})
