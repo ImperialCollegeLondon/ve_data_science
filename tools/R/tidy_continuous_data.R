5b3f10adf352deeff223eceb965d485812feecdb
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-04-29 15:29:48 +0800

    add option to append initial states

--- tools/R/tidy_continuous_data.R
+++ tools/R/tidy_continuous_data.R
@@ -1,50 +1,66 @@
-library(tidyverse)
-library(tidync)
+require(tidync)
+require(tidyverse)
 
-tidy_continuous_data <- function(nc, variables) {
-  cont <- tidync::tidync(nc)
-  cont_dims <- cont |> tidync::hyper_dims()
-  D_space <- cont_dims |> dplyr::filter(name == "cell_id") |> dplyr::pull(id)
-  D_time <- cont_dims |> dplyr::filter(name == "time_index") |> dplyr::pull(id)
+tidy_continuous_data <- function(continuous, variables, initial = NULL) {
+  cont <- tidync(continuous)
+  dims_cont <-
+    list(space = "cell_id", time = "time_index") |>
+    map(\(dim_name) {
+      cont |> hyper_dims() |> filter(name == dim_name) |> pull(id)
+    })
 
-  coords <-
+  coords_cont <-
     cont |>
-    tidync::activate(paste0("D", D_space)) |>
-    tidync::hyper_tibble()
+    activate(paste0("D", dims_cont$space)) |>
+    hyper_tibble()
 
-  time <-
+  time_cont <-
     cont |>
-    tidync::activate(paste0("D", D_time)) |>
-    tidync::hyper_tibble()
+    activate(paste0("D", dims_cont$time)) |>
+    hyper_tibble()
 
-  variables |>
-    purrr::map(\(variable) {
+  tidy_cont <-
+    variables |>
+    map(\(variable) {
       cont |>
-        tidync::activate(variable) |>
-        tidync::hyper_tibble() |>
-        tidyr::pivot_longer(
-          cols = tidyselect::all_of(variable),
+        activate(variable) |>
+        hyper_tibble() |>
+        pivot_longer(
+          cols = all_of(variable),
           names_to = "variable"
         ) |>
-        dplyr::left_join(coords, by = dplyr::join_by(cell_id)) |>
-        dplyr::left_join(time, by = dplyr::join_by(time_index))
+        left_join(coords_cont, by = join_by(cell_id)) |>
+        left_join(time_cont, by = join_by(time_index))
     }) |>
-    purrr::list_c()
+    list_c() |>
+    mutate(time_index = as.numeric(time_index))
+
+  if (!is.null(initial)) {
+    init <- tidync(initial)
+    dims_init <-
+      init |> hyper_dims() |> filter(name == "cell_id") |> pull(id)
+    coords_init <-
+      init |>
+      activate(paste0("D", dims_init)) |>
+      hyper_tibble() |>
+      select(cell_id, x, y)
+
+    tidy_init <-
+      variables |>
+      map(\(variable) {
+        init |>
+          activate(variable) |>
+          hyper_tibble() |>
+          pivot_longer(
+            cols = all_of(variable),
+            names_to = "variable"
+          ) |>
+          left_join(coords_init, by = join_by(cell_id))
+      }) |>
+      list_c() |>
+      mutate(timestamp = -1, time_index = -1)
+
+    tidy_cont <- bind_rows(tidy_cont, tidy_init)
+  }
+  return(tidy_cont)
 }
-
-df_long <-
-  tidy_continuous_data(
-    "data/scenarios/maliau/maliau_2/out/all_continuous_data.nc",
-    c("soil_cnp_pool_pom", "soil_cnp_pool_maom")
-  )
-
-
-init <-
-  tidync("data/scenarios/maliau/maliau_2/out/initial_state.nc")
-
-activate(init, "D1") |> hyper_tibble()
-
-
-ggplot(df_long) +
-  facet_grid(variable ~ element, scales = "free_y") +
-  geom_line(aes(timestamp, value, group = cell_id), alpha = 0.4)
