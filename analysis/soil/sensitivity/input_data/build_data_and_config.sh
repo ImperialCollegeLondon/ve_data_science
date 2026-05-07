4607bc038bceca3b0a1ab3f541d880f68d09d0b9
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-08 07:00:58 +0800

    combine -o and -e

--- analysis/soil/sensitivity/input_data/build_data_and_config.sh
+++ analysis/soil/sensitivity/input_data/build_data_and_config.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #PBS -lwalltime=0:30:00
 #PBS -lselect=1:ncpus=1:mem=8gb
+#PBS -j oe
+#PBS -o /rds/general/user/hlai1/home/logs/build_data_and_config.o$PBS_JOBID
 
 # Initialise conda environment
 eval "$(~/miniforge3/bin/conda shell.bash hook)"
 conda activate r452
-
-# Set custom output/error files (using absolute path)
-#PBS -o /rds/general/user/hlai1/home/logs/build_data_and_config.o$PBS_JOBID
-#PBS -e /rds/general/user/hlai1/home/logs/build_data_and_config.e$PBS_JOBID
 
 # run from the submission directory (typically path/to/ve_data_science)
 cd ${PBS_O_WORKDIR}
