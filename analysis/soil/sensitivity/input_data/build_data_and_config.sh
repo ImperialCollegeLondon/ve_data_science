7cd6c80be0ceebd3806614aaf8e604c1535ad953
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-08 06:22:54 +0800

    rename

--- analysis/soil/sensitivity/input_data/build_data_and_config.sh
+++ analysis/soil/sensitivity/input_data/build_data_and_config.sh
@@ -7,10 +7,11 @@ eval "$(~/miniforge3/bin/conda shell.bash hook)"
 conda activate r452
 
 # Set custom output/error files (using absolute path)
-#PBS -o /rds/general/user/hlai1/home/logs/build_data_sensitivity.o$PBS_JOBID
-#PBS -e /rds/general/user/hlai1/home/logs/build_data_sensitivity.e$PBS_JOBID
+#PBS -o /rds/general/user/hlai1/home/logs/build_data_and_config.o$PBS_JOBID
+#PBS -e /rds/general/user/hlai1/home/logs/build_data_and_config.e$PBS_JOBID
 
 # run from the submission directory (typically path/to/ve_data_science)
 cd ${PBS_O_WORKDIR}
 
 Rscript analysis/soil/sensitivity/input_data/build_data_sensitivity.R
+Rscript analysis/soil/sensitivity/input_data/build_config_sensitivity.R
