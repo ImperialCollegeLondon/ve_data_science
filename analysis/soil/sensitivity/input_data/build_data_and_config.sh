d921df29a09446661ec643cd2227a66083294894
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-08 07:59:29 +0800

    update oe paths

--- analysis/soil/sensitivity/input_data/build_data_and_config.sh
+++ analysis/soil/sensitivity/input_data/build_data_and_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -lwalltime=0:30:00
 #PBS -lselect=1:ncpus=1:mem=8gb
-#PBS -j oe
 #PBS -o /rds/general/user/hlai1/home/logs/build_data_and_config_$PBS_JOBID.o
+#PBS -e /rds/general/user/hlai1/home/logs/build_data_and_config_$PBS_JOBID.e
 
 # Initialise conda environment
 eval "$(~/miniforge3/bin/conda shell.bash hook)"
