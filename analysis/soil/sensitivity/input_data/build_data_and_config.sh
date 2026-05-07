c5b436d54a25126ca3955a5bb46f5ad36a6ff86c
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-08 07:33:54 +0800

    ensure workdir exists

--- analysis/soil/sensitivity/input_data/build_data_and_config.sh
+++ analysis/soil/sensitivity/input_data/build_data_and_config.sh
@@ -2,14 +2,14 @@
 #PBS -lwalltime=0:30:00
 #PBS -lselect=1:ncpus=1:mem=8gb
 #PBS -j oe
-#PBS -o /rds/general/user/hlai1/home/logs/build_data_and_config_${PBS_JOBID}.o
+#PBS -o /rds/general/user/hlai1/home/logs/build_data_and_config_$PBS_JOBID.o
 
 # Initialise conda environment
 eval "$(~/miniforge3/bin/conda shell.bash hook)"
 conda activate r452
 
 # run from the submission directory (typically path/to/ve_data_science)
-cd ${PBS_O_WORKDIR}
+cd "${PBS_O_WORKDIR}" || exit
 
 Rscript analysis/soil/sensitivity/input_data/build_data_sensitivity.R
 Rscript analysis/soil/sensitivity/input_data/build_config_sensitivity.R
