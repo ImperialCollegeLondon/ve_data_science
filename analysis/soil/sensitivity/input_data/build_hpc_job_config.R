41b842cc060a4d20df139a83fa97eb6bdab8199e
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-08 12:22:48 +0800

    ensure config and run number are the same for clarity

--- analysis/soil/sensitivity/input_data/build_hpc_job_config.R
+++ analysis/soil/sensitivity/input_data/build_hpc_job_config.R
@@ -5,17 +5,19 @@ common_config_paths <- list()
 
 site_directory <- "/rds/projects/virtual_rainforest/live/ve_data_science/data/scenarios/sensitivity_soil_litter"
 
+config_list_dir <- "data/scenarios/sensitivity_soil_litter/config"
 config_list_paths <- list.dirs(
-  "data/scenarios/sensitivity_soil_litter/config",
+  config_list_dir,
+  full.names = FALSE,
   recursive = FALSE
 )
 
 jobs <-
   config_list_paths |>
-  imap(\(x, idx) {
+  map(\(x) {
     list(
-      config_paths = list(x),
-      name = paste0("sensitivity_run_", idx),
+      config_paths = list(paste0(config_list_dir, "/", x)),
+      name = paste0("run_", x),
       repeats = 1,
       config = list()
     )
