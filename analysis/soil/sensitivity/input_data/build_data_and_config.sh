81a850b1720c5c14ec5519f07e0f957d6204d351
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-08 11:29:25 +0800

    update out path

--- analysis/soil/sensitivity/input_data/build_data_and_config.sh
+++ analysis/soil/sensitivity/input_data/build_data_and_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -lwalltime=0:30:00
 #PBS -lselect=1:ncpus=1:mem=8gb
-#PBS -o /rds/general/user/hlai1/home/logs/build_data_and_config_$PBS_JOBID.o
-#PBS -e /rds/general/user/hlai1/home/logs/build_data_and_config_$PBS_JOBID.e
+#PBS -j oe
+#PBS -o /rds/general/user/hlai1/home/logs/
 
 # Initialise conda environment
 eval "$(~/miniforge3/bin/conda shell.bash hook)"
@@ -13,6 +13,7 @@ cd "${PBS_O_WORKDIR}" || exit
 
 Rscript analysis/soil/sensitivity/input_data/build_data_sensitivity.R
 Rscript analysis/soil/sensitivity/input_data/build_config_sensitivity.R
+Rscript analysis/soil/sensitivity/input_data/build_hpc_job_config.R
 
 # submit this from the ve_data_science root directory with:
 # qsub analysis/soil/sensitivity/input_data/build_data_and_config.sh
