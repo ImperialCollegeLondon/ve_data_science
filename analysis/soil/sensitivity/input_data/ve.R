1b76b29ae29b4885cd6f487168b47f8e397041a1
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-11 15:38:48 +0800

    switch to job array rather than a single multi-core job

--- analysis/soil/sensitivity/input_data/ve.R
+++ analysis/soil/sensitivity/input_data/ve.R
@@ -1,48 +1,40 @@
 library(tidyverse)
-library(mirai)
 source("tools/R/ve_run.R")
 
+# Capture command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+job_index <- as.numeric(args[1])
 
 # file paths
-config_paths <-
-  list.dirs("data/scenarios/sensitivity_soil_litter/config", recursive = FALSE)
-out_paths <- str_replace(config_paths, "config", "out")
-walk(out_paths, \(path) {
-  if (!dir.exists(path)) {
-    dir.create(path)
-  }
-})
-
-paths <- data.frame(config = config_paths, out = out_paths)
-path_list <- split(paths, 1:nrow(paths))
+config_path <- paste0(
+  "data/scenarios/sensitivity_soil_litter/config/",
+  job_index
+)
+out_path <- paste0(
+  "data/scenarios/sensitivity_soil_litter/out/",
+  job_index
+)
+if (!dir.exists(out_path)) {
+  dir.create(out_path)
+}
 
 # run VE
-daemons(128)
-
-ve_runs <- map(
-  path_list,
-  in_parallel(
-    \(path) {
-      # argument string
-      args <- c(
-        path$config,
-        "--out",
-        path$out,
-        "--logfile",
-        paste0(path$out, "/logfile.log")
-      )
-
-      tryCatch(
-        {
-          ve_run(args, condaenv = "hpc_jobs/virtual_ecosystem_py314")
-        },
-        error = function(e) {
-          message("Error encountered during a run, see log.")
-        }
-      )
-    },
-    ve_run = ve_run
-  )
+args <- c(
+  config_path,
+  "--out",
+  out_path,
+  "--logfile",
+  paste0(out_path, "/logfile.log")
 )
 
-daemons(0)
+tryCatch(
+  {
+    ve_run(args, condaenv = "hpc_jobs/virtual_ecosystem_py314")
+  },
+  error = function(e) {
+    message("Error encountered during a run, see log.")
+  }
+)
+
+# clear unused memory
+gc()
--- analysis/soil/sensitivity/input_data/ve.sh
+++ analysis/soil/sensitivity/input_data/ve.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-#PBS -lwalltime=12:00:00
-#PBS -lselect=1:ncpus=128:mem=512gb
+#PBS -lwalltime=0:30:00
+#PBS -lselect=1:ncpus=1:mem=4gb
+#PBS -J 1-4800%100
 #PBS -j oe
 #PBS -o /rds/general/user/hlai1/home/logs/
 
 # Initialise conda environment
+module purge
 eval "$(~/miniforge3/bin/conda shell.bash hook)"
 conda activate r452
 
 # run from the submission directory (typically path/to/ve_data_science)
 cd "${PBS_O_WORKDIR}" || exit
 
-Rscript analysis/soil/sensitivity/input_data/ve.R
+Rscript analysis/soil/sensitivity/input_data/ve.R $PBS_ARRAY_INDEX
 
 # submit this from the ve_data_science root directory with:
 # cd ../projects/virtual_rainforest/live/ve_data_science
