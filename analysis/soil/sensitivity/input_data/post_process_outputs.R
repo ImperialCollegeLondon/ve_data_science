74566ccf7e5a7db3220794b7760d37721becae9e
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-14 21:28:59 +0800

    switch to mirai

--- analysis/soil/sensitivity/input_data/post_process_outputs.R
+++ analysis/soil/sensitivity/input_data/post_process_outputs.R
@@ -1,39 +1,43 @@
+library(mirai)
 library(tidyverse)
-library(reshape2)
-library(tidync)
-library(futurize)
-source("tools/R/get_all_variables.R")
-
-plan(future.batchtools::batchtools_torque)
 
 out_path <- "data/scenarios/sensitivity_soil_litter/out"
 out_files <- list.files(out_path, "all_continuous_data.nc", recursive = TRUE)
 
+daemons(128)
 merged <- map(
-  out_files[1:2],
-  \(file) {
-    tidync(file.path(out_path, file)) |>
-      get_all_variables() |>
-      melt() |>
-      mutate(
-        scenario = sub("/.*", "", file),
-        value = as.numeric(value)
-      ) |>
-      filter(!is.na(value)) |>
-      unite(
-        "variable",
-        L1,
-        element,
-        layers,
-        groundwater_layers,
-        pft,
-        na.rm = TRUE,
-        remove = TRUE
-      ) |>
-      select(-cell_id)
-  }
+  out_files,
+  in_parallel(
+    \(out_file) {
+      require(tidyverse)
+      require(reshape2)
+      require(tidync)
+      source("tools/R/get_all_variables.R")
+
+      tidync(file.path(out_path, out_file)) |>
+        get_all_variables() |>
+        melt() |>
+        mutate(
+          scenario = sub("/.*", "", out_file),
+          value = as.numeric(value)
+        ) |>
+        filter(!is.na(value)) |>
+        unite(
+          "variable",
+          L1,
+          element,
+          layers,
+          groundwater_layers,
+          pft,
+          na.rm = TRUE,
+          remove = TRUE
+        ) |>
+        select(-cell_id)
+    },
+    out_path = out_path
+  )
 ) |>
-  futurize(options = futurize_options(packages = c("purrr"))) |>
   list_rbind()
+daemons(0)
 
 write_csv(merged, file.path(out_path, "all_continuous_data_merged.nc"))
