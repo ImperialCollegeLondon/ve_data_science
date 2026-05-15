97db1e2d2b836df7342911fba56105574d3a53c8
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-15 16:48:02 +0800

    scp template

--- analysis/soil/sensitivity/input_data/post_process_outputs.sh
+++ analysis/soil/sensitivity/input_data/post_process_outputs.sh
@@ -17,3 +17,4 @@ Rscript analysis/soil/sensitivity/input_data/post_process_outputs.R
 # submit this from the ve_data_science root directory with:
 # cd ../projects/virtual_rainforest/live/ve_data_science
 # qsub analysis/soil/sensitivity/input_data/post_process_outputs.sh
+# scp hlai1@dtn-c.cx3.hpc.ic.ac.uk:/rds/general/user/hlai1/projects/virtual_rainforest/live/ve_data_science/data/scenarios/sensitivity_soil_litter/out/all_continuous_data_merged.parquet data/scenarios/sensitivity_soil_litter/out/all_continuous_data_merged.parquet
