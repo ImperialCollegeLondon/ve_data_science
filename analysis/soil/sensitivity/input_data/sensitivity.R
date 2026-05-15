6cae716c589767f01a8dbee4c0ecc90f720f1307
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-15 16:49:32 +0800

    draft the overall calculation and visualisation of sensitivity analysis

--- analysis/soil/sensitivity/input_data/sensitivity.R
+++ analysis/soil/sensitivity/input_data/sensitivity.R
@@ -1,9 +1,52 @@
+library(tidyverse)
 library(sensobol)
-
+library(tidync)
+library(arrow)
+source("tools/R/sobol_indices2.R")
 
 sobol_mat <- read_rds(
   "data/scenarios/sensitivity_soil_litter/data/sobol_matrix.rds"
 )
 pars <- colnames(sobol_mat)
+n_samples <- nrow(sobol_mat) / (length(pars) + 2)
 
-ind <- sobol_indices(Y = 1:nrow(sobol_mat), N = n_sample, params = pars)
+merged <-
+  open_dataset(
+    "data/scenarios/sensitivity_soil_litter/out/all_continuous_data_merged.parquet"
+  ) |>
+  filter(time_index == 131) %>%
+  collect()
+
+Y_vars <- unique(merged$variable) |> setdiff(c("timestamp"))
+
+sens <- map(
+  Y_vars,
+  \(Y_var) {
+    Y <-
+      merged |>
+      filter(variable == Y_var) |>
+      mutate(scenario = as.numeric(scenario)) |>
+      right_join(
+        data.frame(scenario = seq_len(nrow(sobol_mat))),
+        by = join_by(scenario)
+      ) |>
+      arrange(scenario) |>
+      pull(value)
+    sobol_indices2(Y = Y, N = n_samples, params = pars) |>
+      pluck("results") |>
+      as.data.frame() |>
+      mutate(Y_var = Y_var)
+  },
+  .progress = TRUE
+) |>
+  list_rbind()
+
+
+sens |>
+  filter(sensitivity == "Ti") |>
+  ggplot() +
+  geom_raster(aes(y = parameters, x = Y_var, fill = original)) +
+  scale_fill_viridis_c(option = "cividis", transform = "sqrt") +
+  coord_fixed() +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
