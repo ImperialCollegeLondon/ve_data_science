4985a65810f52aa1e7792fe4e613689582cfdeaf
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-20 16:57:39 +0800

    export dynamic file path and array index

--- analysis/soil/sensitivity/input_data/ve.sh
+++ analysis/soil/sensitivity/input_data/ve.sh
@@ -13,7 +13,12 @@ conda activate r452
 # run from the submission directory (typically path/to/ve_data_science)
 cd "${PBS_O_WORKDIR}" || exit
 
-Rscript analysis/soil/sensitivity/input_data/ve.R $PBS_ARRAY_INDEX
+# Export per-job filename (should I stage to temporary directory?)
+export SOIL_LITTER_DATA="data/scenarios/sensitivity_soil_litter/data/soil_litter_data_${PBS_ARRAY_INDEX}.nc"
+export PBS_ARRAY_INDEX
+
+# run R script
+Rscript analysis/soil/sensitivity/input_data/ve.R
 
 # submit this from the ve_data_science root directory with:
 # cd ../projects/virtual_rainforest/live/ve_data_science
