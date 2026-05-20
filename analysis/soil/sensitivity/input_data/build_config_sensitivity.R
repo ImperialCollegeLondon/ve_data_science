69ea962ece8316db3a13a13569f3ae45bd6f935c
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-05-20 16:54:50 +0800

    refactor config to avoid file duplication with dynamic variable path

--- analysis/soil/sensitivity/input_data/build_config_sensitivity.R
+++ analysis/soil/sensitivity/input_data/build_config_sensitivity.R
@@ -1,42 +1,35 @@
 library(tidyverse)
 library(toml)
-library(mirai)
 source("tools/R/build_config.R")
 source("tools/R/collect_data_paths.R")
 
 
 # Define config values -----------------------------------
 
-# new values for core.grid
-core_grid <-
+# core values
+core <-
   read_toml("data/derived/site/maliau/maliau_grid_definition.toml") |>
   pluck("Scenario") |>
   pluck("maliau_1") |>
-  pluck("core") |>
-  pluck("grid") |>
+  pluck("core")
+# values for core.grid
+core_grid <-
+  core$grid |>
   # modify values
   list_modify(cell_nx = 1, cell_ny = 1)
+# values for core.timing
+core_timing <- core$timing
 
 # input data paths for core.data.variable
-# first list the soil and litter data files to loop over
-data_paths_soil_litter <- paste0(
-  "../../data/",
-  list.files(
-    "data/scenarios/sensitivity_soil_litter/data",
-    "soil_litter_data",
-  )
+# the soil and litter data paths are placeholder environment variables
+# that will be specified in the HPC job array script later
+data_paths <- collect_data_paths(
+  plants = "../data/plant_input_data_Maliau_50x50_mean.nc",
+  climate = "../data/era5_maliau_2010_2020_100m_mean.nc",
+  elevation = "../data/elevation_maliau_2010_2020_100m_mean.nc",
+  soil = "$SOIL_LITTER_DATA",
+  litter = "$SOIL_LITTER_DATA"
 )
-data_paths <-
-  data_paths_soil_litter |>
-  map(\(path) {
-    collect_data_paths(
-      plants = "../../data/plant_input_data_Maliau_50x50_mean.nc",
-      climate = "../../data/era5_maliau_2010_2020_100m_mean.nc",
-      elevation = "../../data/elevation_maliau_2010_2020_100m_mean.nc",
-      soil = path,
-      litter = path
-    )
-  })
 
 # new values for plants.constants
 plants_contants <-
@@ -49,82 +42,55 @@ plants_contants <-
 # These new values reflect the Maliau scenarios
 
 # core config
-core_list <-
-  map(
-    data_paths,
-    \(data_path) {
-      list(
-        grid = core_grid,
-        timing = list(
-          start_date = "2010-01-01",
-          update_interval = "1 month",
-          run_length = "11 years"
-        ),
-        data_output_options = list(
-          save_initial_state = TRUE,
-          save_continuous_data = TRUE
-        ),
-        data = list(variable = data_path)
-      )
-    }
-  )
+core_config <- list(
+  grid = core_grid,
+  timing = core_timing,
+  data_output_options = list(
+    save_initial_state = TRUE,
+    save_continuous_data = TRUE
+  ),
+  data = list(variable = data_paths)
+)
 
 # plants config
-plants <- list(
-  cohort_data_path = "../../data/plant_cohort_data_Maliau_50x50.csv",
-  pft_definitions_path = "../../data/plant_pft_definitions_Maliau_50x50.csv",
+plants_config <- list(
+  cohort_data_path = "../data/plant_cohort_data_Maliau_50x50.csv",
+  pft_definitions_path = "../data/plant_pft_definitions_Maliau_50x50.csv",
   constants = as.list(plants_contants)
 )
 
 # animal config
-animal <- list(
-  functional_group_definitions_path = "../../data/animal_functional_groups_Maliau_level1.csv",
+animal_config <- list(
+  functional_group_definitions_path = "../data/animal_functional_groups_Maliau_level1.csv",
   cohort_data_export = list(enabled = FALSE)
 )
 
 
 # Build configuration files ----------------------------------------------
+config_dir <- "data/scenarios/sensitivity_soil_litter/config"
+if (!dir.exists(config_dir)) {
+  dir.create(config_dir)
+} else {
+  message("All good, config directory already exists.")
+}
 
-daemons(16)
-
-iwalk(
-  core_list,
-  in_parallel(
-    \(x, idx) {
-      config_dir <- paste0(
-        "data/scenarios/sensitivity_soil_litter/config/",
-        idx
-      )
-      if (!dir.exists(config_dir)) {
-        dir.create(config_dir)
-      }
-      build_config(
-        requested_modules = c(
-          "core",
-          "abiotic_simple",
-          "hydrology",
-          "plants",
-          "animal",
-          "soil",
-          "litter"
-        ),
-        core = x,
-        plants = plants,
-        animal = animal,
-        path = config_dir
-      )
-      # Copy over soil microbial config that does not change across scenarios
-      file.copy(
-        "data/scenarios/maliau/maliau_1/config/soil_microbial_groups.toml",
-        config_dir
-      )
-    },
-    build_config = build_config,
-    write_toml = toml::write_toml,
-    export_config = export_config,
-    plants = plants,
-    animal = animal
-  )
+build_config(
+  requested_modules = c(
+    "core",
+    "abiotic_simple",
+    "hydrology",
+    "plants",
+    "animal",
+    "soil",
+    "litter"
+  ),
+  core = core_config,
+  plants = plants_config,
+  animal = animal_config,
+  path = config_dir
 )
-
-daemons(0)
+# Copy over soil microbial config that does not change across scenarios
+file.copy(
+  "data/scenarios/maliau/maliau_1/config/soil_microbial_groups.toml",
+  config_dir
+)
