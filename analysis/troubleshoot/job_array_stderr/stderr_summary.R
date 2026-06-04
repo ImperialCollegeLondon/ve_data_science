7d1c351cb8af779a72451d5d9267394bc2763e05
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-06-04 15:32:42 +0800

    sketch into map

--- analysis/troubleshoot/job_array_stderr/stderr_summary.R
+++ analysis/troubleshoot/job_array_stderr/stderr_summary.R
@@ -6,6 +6,7 @@ library(arrow)
 n_runs <- 4800
 job_id <- "2891288"
 log_dir <- "../hpc_logs/logs"
+tail_n <- 5
 
 # retrieve the index of successful runs from the merged dataset
 successful_runs <-
@@ -19,11 +20,19 @@ successful_runs <-
 # get a list of failed runs
 failed_runs <- setdiff(seq_len(n_runs), successful_runs)
 
-map_chr(failed_runs, \(run_id) {
-  paste0(job_id, "[", run_id, "].pbs-7.ER")
-})
+errors <-
+  map(
+    failed_runs,
+    \(run_id) {
+      logfile <- file.path(log_dir, paste0(job_id, "[", run_id, "].pbs-7.ER"))
+      if (file.exists(logfile)) {
+        tail(read_lines(logfile), tail_n)
+      } else {
+        NA_character_
+      }
+    },
+    .progress = TRUE
+  )
 
-run_id <- 1919
-logfile <- file.path(log_dir, paste0(job_id, "[", run_id, "].pbs-7.ER"))
-logtext <- read_lines(logfile)
-tail(logtext, 3)
+# Remove NA elements
+errors2 <- discard(errors, \(x) all(is.na(x)))
