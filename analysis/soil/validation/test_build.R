9c04eac6313ff0cc01e835d10192d1d1fc79796e
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-08-04 07:29:09 +0800

    include x, y, and timestamp

--- analysis/soil/validation/test_build.R
+++ analysis/soil/validation/test_build.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(reshape2)
 library(arrow)
 library(pizzarr)
 box::use(tools/R/R/get_ve_variables[...])
@@ -18,9 +19,28 @@ config_path <- "data/scenarios/maliau/maliau_2/out/compiled_configuration.toml"
 # outputs <- zarr_open(zarr_path)
 # outputs_group <- outputs$get_item("outputs")
 
+xy <-
+  get_data_variables(zarr_path, group = "outputs", variables = c("x", "y")) |>
+  melt() |>
+  pivot_wider(names_from = L1, values_from = value)
+
+time <-
+  get_data_variables(
+    zarr_path,
+    group = "outputs",
+    variables = c("timestamp")
+  ) |>
+  melt() |>
+  pivot_wider(names_from = L1, values_from = value)
+
+
 vars_derived <- get_derived_variables(
   zarr_path,
   config_path,
   group = "outputs",
   variables = vars[-which(vars == "groundwater_storage")]
-)
+) |>
+  melt() |>
+  rename(var_canonical = L1) |>
+  left_join(xy) |>
+  left_join(time)
