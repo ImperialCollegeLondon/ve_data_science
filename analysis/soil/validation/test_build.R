565c85050ea5bfaa5ee22b4d65025befb12d58c0
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-08-06 12:21:29 +0800

    write combined database

--- analysis/soil/validation/test_build.R
+++ analysis/soil/validation/test_build.R
@@ -2,14 +2,20 @@ library(dplyr)
 library(arrow)
 box::use(tools/R/R/valdb)
 
-
-# Usage -------------------------------------------------------------------
-
+# Paths to the VE scenario simulation outputs
 zarr_path <- "data/scenarios/maliau/maliau_2/out/model_data.zarr"
 config_path <- "data/scenarios/maliau/maliau_2/out/compiled_configuration.toml"
 
+# Read the validation database
 db_path <- "data/derived/soil/validation/database"
 validation_database <- open_dataset(db_path) |> collect()
 
+# Combine the validation database with VE outputs
 combined_database <-
   valdb$join_ve_outputs(validation_database, zarr_path, config_path)
+
+# Write the combined database
+combined_db_path <- "data/derived/soil/validation/database_combined"
+combined_database |>
+  group_by(dataset) |>
+  write_dataset(combined_db_path, format = "parquet")
