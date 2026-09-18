fab6ba3cfed40079e9fbb3730c7202038f2d13f4
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-09-18 09:47:37 +0800

    move screening chunk

--- analysis/soil/validation/openalex.R
+++ analysis/soil/validation/openalex.R
@@ -1,8 +1,24 @@
 library(tidyverse)
-library(openalexR)
-library(yaml)
+box::use(tools/R/R/valdb)
 
-results <- oa_fetch(
-  title_and_abstract.search.exact = "('Maliau Basin' OR Sabah) AND (soil OR litter OR microb* OR bacteri* OR fungi OR fungal OR decompos* OR necromass OR deadwood OR 'dead wood' OR carbon OR nitrogen OR phosphorus)",
-  verbose = TRUE
-)
+
+# Literature search on OpenAlex ------------------------------------------
+
+results_path <- "data/derived/soil/validation/literature_search/results.csv"
+
+if (file.exists(results_path)) {
+  results <- read_csv(results_path, show_col_types = FALSE)
+} else {
+  results <- openalexR::oa_fetch(
+    title_and_abstract.search.exact = "('Maliau Basin' OR Sabah) AND (soil OR litter OR microb* OR bacteri* OR fungi OR fungal OR decompos* OR necromass OR deadwood OR 'dead wood' OR carbon OR nitrogen OR phosphorus)",
+    verbose = TRUE
+  )
+
+  write_csv(results, results_path)
+}
+
+# Screen OpenAlex search results -----------------------------------------
+
+sources_dir <- here::here("data/derived/soil/validation/sources")
+
+valdb$screen_dataset(sources_dir = sources_dir)
--- analysis/soil/validation/screening.R
+++
@@ -1,4 +0,0 @@
-box::use(tools/R/R/valdb)
-sources_dir <- here::here("data/derived/soil/validation/sources")
-
-valdb$screen_dataset(sources_dir = sources_dir)
