a7ab472fcaa5fe811ce01b92ace33c65dbd999d5
moi-taiga <moi.t.nicholas@gmail.com>
2026-09-23 22:09:50 +0100

    VEco outputs are generated in /rds/general/user/mtnichol/ephemeral before being moved to out_dir

--- hpc_jobs/run_subJob.py
+++ hpc_jobs/run_subJob.py
@@ -3,6 +3,7 @@
 import os
 import argparse 
 from pathlib import Path
+import shutil
 
 from virtual_ecosystem.main import ve_run
 
@@ -43,11 +44,7 @@ output_dir = args.output_dir.resolve()
 with arrayJob_config_file.open("rb") as array_job_file:
     arrayJob_spec = load_arrayJob_spec(array_job_file)
 
-# the output however contains MANY files
-# so we will likley need to stage the output directory (set to $TMPDIR)
-# then compress into tarball and copy back to the final output location
-# generating compressed zarr may resolve this issue.
-# in the meantime... for small jobs, work from site dir.
+# move to the site directory so any relative paths work correctly
 os.chdir(arrayJob_spec.site_directory)
 
 # Extract the job from the jobs spec by index
@@ -62,14 +59,33 @@ if not output_dir.is_dir():
     raise NotADirectoryError(f"Sub-job output directory not found: \n{output_dir}")
 
 # Update the configuration to set the output directory for this sub-job.
+# Use $TMPDIR as a staging area (due to the high file number of ZARR)
+# Make a unique subdirectory for this sub-job within the staging area
+staging_dir = Path(os.environ["TMPDIR"]).resolve() 
+staging_dir = staging_dir / f"array_subJob_{pbs_array_index}"
+staging_dir.mkdir(parents=True, exist_ok=True)
+
 core_config = cli_config.setdefault("core", {})
 output_options = core_config.setdefault("data_output_options", {})
-output_options["out_path"] = str(output_dir)
-# print(f"[DEBUG] cli_config: {cli_config}", flush=True)
+output_options["out_path"] = str(staging_dir)
 
-# 5. Start the run
-ve_run(
-    cfg_paths=config_paths,
-    cli_config=cli_config,
-    logfile=output_dir / "ve.log",
-)
+# using the try block to ensure cleanup of the staging directory
+# even when ve_run fails.
+try:
+
+    # 5. Start the run
+    ve_run(
+        cfg_paths=config_paths,
+        cli_config=cli_config,
+        logfile=staging_dir / "ve.log",
+        to_netcdf=True,
+    )
+finally:
+    # remove the temporary Zarr output to reduce disk usage
+    shutil.rmtree(staging_dir / "model_data.zarr", ignore_errors=True)
+
+    # move outputs from the staging directory to the final output directory
+    # this assumes the output_dir doesnt exist, but earlier we checked that it does not
+    for item in staging_dir.iterdir():
+        shutil.move(str(item), output_dir / item.name)
+    staging_dir.rmdir()
